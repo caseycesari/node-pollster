64aba6aea03b0f7575b3f77bb7819a361e69d307
Casey Thomas <cpthomas@gmail.com>
2012-09-15 18:46:48 -0400

    resp not result

--- test/test.js
+++ test/test.js
@@ -1,13 +1,17 @@
 var pollster = require('../lib/pollster');
 
-pollster.charts({'state': 'PA'}, function(result){
-  console.log(result);
+pollster.charts({'state': 'PA'}, function(resp){
+  console.log(resp);
 });
 
-pollster.chart('2012-pennsylvania-gop-primary', function(result){
-  console.log(result);
+pollster.chart('2012-pennsylvania-gop-primary', function(resp){
+  console.log(resp);
 });
 
-pollster.polls({'state': 'PA', 'topic': '2012-house'}, function(result){
-  console.log(result);
+pollster.polls({'state': 'PA', 'topic': '2012-house'}, function(resp){
+  console.log(resp);
+});
+
+pollster.polls({chart:'us-health-bill'}, function(resp){
+  console.log(resp.map(function(p) { return [p.pollster, p.method ]; }));
 });
