d74deabc89569ada2fb23ab95d8fcb76db1e5e8f
Casey Thomas <cpthomas@gmail.com>
2012-09-15 15:48:55 -0400

    uncomment

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
 var pollster = require('../lib/pollster');
 
-/*pollster.charts({'state': 'PA'}, function(result){
+pollster.charts({'state': 'PA'}, function(result){
   console.log(result);
 });
 
 pollster.chart('2012-pennsylvania-gop-primary', function(result){
   console.log(result);
-});*/
+});
 
 pollster.polls({'state': 'PA', 'topic': '2012-house'}, function(result){
   console.log(result);
